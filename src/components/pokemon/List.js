3d737a966219d9ab4e0a0075e90b29a237675071
wellsousa1987 <wellsousa1987@gmail.com>
2021-11-04 23:28:42 -0300

    inclusao da lib axios para trabalhar com requests

--- src/components/pokemon/List.js
+++ src/components/pokemon/List.js
@@ -1,9 +1,20 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 
 import Card from './Card';
 
 export default class List extends Component{
+
+    state = {
+        url: "https://pokeapi.co/api/v2/pokemon/",
+        pokemon: null,
+    };
+
+    componentDidMount(){
+
+    };
+
     render(){
         return(
             <div className="row">
